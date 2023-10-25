9898b3b8aeb7c204f5ad9023bf21a7655295e25e
xiaoxia <lx0112@outlook.com>
2023-10-25 20:14:47 +0800

    转换仓库初始化

--- game.c
+++ game.c
@@ -9,17 +9,48 @@ void initBoard(char board[Row][Col], int row, int col) {
 
     for (int i = 0; i < row; i++) {
         for (int i1 = 0; i1 < col; i1++) {
-            board[i][i1] =' ';
+            board[i][i1] = ' ';
         }
     }
 }
+
+
 
 void displayBoard(char pString[Row][Col], int row, int col) {
     for (int i = 0; i < row; i++) {
-        for (int i1 = 0; i1 < col; i1++) {
-            printf("%c", pString[i][i1]);
+
+        for (int j = 0; j < col; j++){
+
+            printf(" %c ", pString[i][j]);
+            if (j < col - 1) {
+                printf("|");
+            }
         }
         printf("\n");
+
+        if (i < row - 1) {
+            for (int z = 0; z < col; z++){
+
+                printf("---");
+                if (z < row - 1) {
+
+                    printf("|");
+                }
+            }
+            printf("\n");
+        }
     }
 
 }
+//
+//void displayBoard(char pString[Row][Col], int row, int col) {
+//    for (int i = 0; i < row; i++) {
+//
+//        printf(" %c | %c | %c \n", pString[i][0], pString[i][1], pString[i][2]);
+//        if (i < row - 1) {
+//
+//            printf("---|---|--- \n");
+//        }
+//    }
+//
+//}
--- main.c
+++ main.c
@@ -6,10 +6,20 @@ void startGame();
 void menu();
 
 
-
-
 int main() {
-    int input = 0;
+
+
+
+
+ int a = 0;
+
+    char ch[10] = {0};
+
+
+    printf("%d\n", sizeof(ch));
+
+
+
     /*do {
 
         menu();
@@ -21,32 +31,35 @@ int main() {
                 break;
 
             case 0:
-                printf("%s\n", "退出游戏!");
+                printf("%s\n", "退出")—;
                 break;
             default:
-                printf("%s\n", "输入错误");
+                printf("%s\n", "输入错误请重新输入");
                 break;
         }
 
     } while (input);*/
 
-    startGame();
+//    startGame();
     return 0;
 }
 
 void menu() {
 
-    printf("%s\n", "****游戏菜单******");
+    printf("%s\n", "****五子棋******");
 
-    printf("%s\n", "1. 开始游戏 0.退出");
+    printf("%s\n", "1.开始游戏 0.退出游戏");
 }
 
 void startGame() {
     char board[Row][Col] = {0};
 
-//    初始化棋盘
+
     initBoard(board, Row, Col);
     displayBoard(board, Row, Col);
+
+   
+
 
 }
 
