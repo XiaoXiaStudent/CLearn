75e8aad55be6da6c41a9e08257e6110949ea2a8c
youzi <youzi@outlook.com>
2023-10-26 14:17:14 +0800

    结构体章节结束

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 
 #include "game.h"
-
+#include "string.h"
 void startGame();
 
 void menu();
+
+void gameStart();
+
+void pointer();
 
 int my_strlen(char* str) {
     // 如果当前字符是结束符 '\0'，则返回0
@@ -14,28 +18,52 @@ int my_strlen(char* str) {
     return 1 + my_strlen(str + 1);
 }
 
+// 定义书的结构体
+struct Book {
+    char title[50];   // 书名
+    char author[50];  // 作者名
+    float price;      // 价格
+};
+
 
 int main() {
 
 
-    int len = my_strlen("abcdef");
-    printf("%d\n", len);
+//    int len = my_strlen("ab11cdef");
+//    printf("%d\n", len);
+
+    // 初始化一个Book结构体变量
+    struct Book myBook;
+
+    strcpy(myBook.title, "C Programming for Experts");
+    strcpy(myBook.author, "John Doe");
+    myBook.price = 29.99;
+
+    // 输出书的信息
+    printf("Book Information:\n");
+    printf("Title: %s\n", myBook.title);
+    printf("Author: %s\n", myBook.author);
+    printf("Price: $%.2f\n", myBook.price);
+
     return 0;
 
 
-    /**
-     * 在这段代码中，int* pa 和 pa 有不同的含义：
 
-    pa 是一个指针变量，用于存储一个整数的地址。当你执行 int* pa = &a;，你实际上是将变量 a 的内存地址赋值给 pa。
+}
 
-    *pa 是解引用操作。当你使用 *pa，你实际上是访问 pa 所指向的内存地址中的内容。在这个例子中，*pa 的值就是变量 a 的值，也就是 10。
+void pointer() {/**
+ * 在这段代码中，int* pa 和 pa 有不同的含义：
 
-    所以：
+pa 是一个指针变量，用于存储一个整数的地址。当你执行 int* pa = &a;，你实际上是将变量 a 的内存地址赋值给 pa。
 
-    pa 是变量 a 的地址。
-    *pa 是存储在该地址的值，即 10。
-    另外，关于 pa + 1;：这个操作增加了 pa 指针的值，但增加的量取决于它所指向的数据类型的大小。因为 pa 是一个 int 指针，并且通常情况下 int 是 4 字节，所以 pa + 1 会使指针向后移动 4 个字节。但是在这段代码中，您没有将结果赋值给 pa，所以 pa 的值不会改变。
-         */
+*pa 是解引用操作。当你使用 *pa，你实际上是访问 pa 所指向的内存地址中的内容。在这个例子中，*pa 的值就是变量 a 的值，也就是 10。
+
+所以：
+
+pa 是变量 a 的地址。
+*pa 是存储在该地址的值，即 10。
+另外，关于 pa + 1;：这个操作增加了 pa 指针的值，但增加的量取决于它所指向的数据类型的大小。因为 pa 是一个 int 指针，并且通常情况下 int 是 4 字节，所以 pa + 1 会使指针向后移动 4 个字节。但是在这段代码中，您没有将结果赋值给 pa，所以 pa 的值不会改变。
+     */
     int a = 10;
 
     int* pa = &a;
@@ -46,26 +74,27 @@ int main() {
 
 
     printf("%p\n", (void*)pa);
-    return 0;
-    /*do {
+}
 
-        menu();
-        scanf("%d", &input);
+void gameStart() {/*do {
 
-        switch (input) {
-            case 1:
-                startGame();
-                break;
+    menu();
+    scanf("%d", &input);
 
-            case 0:
-                printf("%s\n", "退出")—;
-                break;
-            default:
-                printf("%s\n", "输入错误请重新输入");
-                break;
-        }
+    switch (input) {
+        case 1:
+            startGame();
+            break;
 
-    } while (input);*/
+        case 0:
+            printf("%s\n", "退出")—;
+            break;
+        default:
+            printf("%s\n", "输入错误请重新输入");
+            break;
+    }
+
+} while (input);*/
 
 //    startGame();
 
